4b2f19169ec8db476884df3ed6008f7d754b7b06
haoyu <haoyuhua@usc.edu>
2020-12-18 20:15:54 -0800

    fix stats

--- RocksDBConfig.cmake
+++ RocksDBConfig.cmake
@@ -3,17 +3,7 @@
 ####### Any changes to this file will be overwritten by the next CMake run ####
 ####### The input file was RocksDBConfig.cmake.in                            ########
 
-get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../../" ABSOLUTE)
-
-# Use original install prefix when loaded through a "/usr move"
-# cross-prefix symbolic link such as /lib -> /usr/lib.
-get_filename_component(_realCurr "${CMAKE_CURRENT_LIST_DIR}" REALPATH)
-get_filename_component(_realOrig "/usr/lib/x86_64-linux-gnu/cmake/rocksdb" REALPATH)
-if(_realCurr STREQUAL _realOrig)
-  set(PACKAGE_PREFIX_DIR "/usr")
-endif()
-unset(_realOrig)
-unset(_realCurr)
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
 
 macro(set_and_check _var _file)
   set(${_var} "${_file}")
@@ -38,7 +28,7 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/modules")
 
 include(CMakeFindDependencyMacro)
 
-set(GFLAGS_USE_TARGET_NAMESPACE ON)
+set(GFLAGS_USE_TARGET_NAMESPACE )
 
 if(OFF)
   find_dependency(JeMalloc)
--- build_version.cc
+++ build_version.cc
@@ -1,5 +1,5 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@566ee7ccbaea7b1806f0a79fdc1ebc4331bfec0b@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2020/12/18 17:43:33@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@26a09942eacf861018c9645695eaea4cbdd8dba6@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2021/01/02 15:40:01@";
 const char* rocksdb_build_compile_date = __DATE__;
